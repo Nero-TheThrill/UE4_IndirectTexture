18078757ae78ec396c06207f39eb29eead9507a9
Jinwoo <imjinwoo98@gmail.com>
2025-01-08 17:49:08 +0900

    clean project

--- IndirectTexture/Intermediate/Build/Win64/IndirectTextureEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:/UE4_IndirectTexture/IndirectTexture/Intermediate/Build/Win64/IndirectTextureEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- IndirectTexture/Source/IndirectTexture/IndirectTexture.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "IndirectTexture.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IndirectTexture, "IndirectTexture" );
